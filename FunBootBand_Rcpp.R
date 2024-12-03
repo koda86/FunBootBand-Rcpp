beb0b24a3a56c19497fbd53e5e7e7e3d53aab0bb
Daniel Koska <koda86@users.noreply.github.com>
2024-12-03 14:53:04 +0100

    minor change in comment

--- FunBootBand_Rcpp.R
+++ FunBootBand_Rcpp.R
@@ -3,7 +3,7 @@ library(Rcpp)
 
 data <- read.csv("~/FunBootBand-Rcpp/example_data.csv")
 
-# Load the R-Cpp source file created/stored in FunBootBand_cpp.cpp
+# Load the functions created in the R-Cpp source file (FunBootBand_cpp.cpp)
 sourceCpp("~/FunBootBand-Rcpp/FunBootBand_cpp.cpp")
 
 # Testweise Laden der Funtion getDimensions
